7a16c3a52f2f65c7a1984367fb125ea95c29d05b
nadka :) <101661628+nadkka@users.noreply.github.com>
2023-01-14 10:16:23 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,309 @@
+document.onselectstart = function(){return false;};
+
+function piekarnia1(){
+    document.getElementById('bok').innerHTML+='<img src="bakery.png" class="moving" height="100"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function piekarnia2(){
+    document.getElementById('bok').innerHTML+='<img src="bakery2.png" class="moving" height="100"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function bank1(){
+    document.getElementById('bok').innerHTML+='<img src="bank.png" class="moving" height="150"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function bank2(){
+    document.getElementById('bok').innerHTML+='<img src="bank2.png" class="moving" height="150"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function kosciol1(){
+    document.getElementById('bok').innerHTML+='<img src="church.png" class="moving" height="150"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function kosciol2(){
+    document.getElementById('bok').innerHTML+='<img src="church2.png" class="moving" height="150"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function cyrk1(){
+    document.getElementById('bok').innerHTML+='<img src="wm.png" class="moving" height="60"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function cyrk2(){
+    document.getElementById('bok').innerHTML+='<img src="wm2.png" class="moving" height="100"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function cyrk3(){
+    document.getElementById('bok').innerHTML+='<img src="wm3.png" class="moving" height="200"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function cyrk4(){
+    document.getElementById('bok').innerHTML+='<img src="wm4.png" class="moving" height="100"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function cyrk5(){
+    document.getElementById('bok').innerHTML+='<img src="wm5.png" class="moving" height="200"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function cyrk6(){
+    document.getElementById('bok').innerHTML+='<img src="wm6.png" class="moving" height="300"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function school1(){
+    document.getElementById('bok').innerHTML+='<img src="school.png" class="moving" height="150"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function school2(){
+    document.getElementById('bok').innerHTML+='<img src="school2.png" class="moving" height="150"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function straz1(){
+    document.getElementById('bok').innerHTML+='<img src="straz.png" class="moving" height="150"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function straz2(){
+    document.getElementById('bok').innerHTML+='<img src="straz2.png" class="moving" height="150"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function dom1(){
+    document.getElementById('bok').innerHTML+='<img src="dom.png" class="moving" height="100"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function dom2(){
+    document.getElementById('bok').innerHTML+='<img src="dom2.png" class="moving" height="100"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function dom3(){
+    document.getElementById('bok').innerHTML+='<img src="dom3.png" class="moving" height="100"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function dom4(){
+    document.getElementById('bok').innerHTML+='<img src="dom4.png" class="moving" height="100"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function dom5(){
+    document.getElementById('bok').innerHTML+='<img src="dom5.png" class="moving" height="100"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function dom6(){
+    document.getElementById('bok').innerHTML+='<img src="dom6.png" class="moving" height="100"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function dom7(){
+    document.getElementById('bok').innerHTML+='<img src="dom7.png" class="moving" height="100"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function dom8(){
+    document.getElementById('bok').innerHTML+='<img src="dom8.png" class="moving" height="100"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function dom9(){
+    document.getElementById('bok').innerHTML+='<img src="dom9.png" class="moving" height="100"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function szpital1(){
+    document.getElementById('bok').innerHTML+='<img src="szpital.png" class="moving" height="160"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function szpital2(){
+    document.getElementById('bok').innerHTML+='<img src="szpital2.png" class="moving" height="160"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function szpital3(){
+    document.getElementById('bok').innerHTML+='<img src="szpital3.png" class="moving" height="200"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function ch1(){
+    document.getElementById('bok').innerHTML+='<img src="centr.png" class="moving" height="160"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function ch2(){
+    document.getElementById('bok').innerHTML+='<img src="centr2.png" class="moving" height="160"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function mechanik1(){
+    document.getElementById('bok').innerHTML+='<img src="mechanic.png" class="moving" height="130"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function park1(){
+    document.getElementById('bok').innerHTML+='<img src="park.png" class="moving" height="100"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function park2(){
+    document.getElementById('bok').innerHTML+='<img src="park2.png" class="moving" height="100"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function parking1(){
+    document.getElementById('bok').innerHTML+='<img src="spaces.png" class="moving" height="100"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function droga1(){
+    document.getElementById('bok').innerHTML+='<img src="droga.png" class="moving" width="100"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function droga2(){
+    document.getElementById('bok').innerHTML+='<img src="droga2.png" class="moving" height="100"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function zakret1(){
+    document.getElementById('bok').innerHTML+='<img src="skret.png" class="moving" height="105"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function zakret2(){
+    document.getElementById('bok').innerHTML+='<img src="skret2.png" class="moving" height="105"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function zakret3(){
+    document.getElementById('bok').innerHTML+='<img src="skret3.png" class="moving" height="105"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function zakret4(){
+    document.getElementById('bok').innerHTML+='<img src="skret4.png" class="moving" height="105"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function pdp1(){
+    document.getElementById('bok').innerHTML+='<img src="crosswalk.png" class="moving" width="50"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function pdp2(){
+  document.getElementById('bok').innerHTML+='<img src="crosswalk2.png" class="moving" height="50"></img>';
+  $( function() {     
+      $( ".moving" ).draggable();
+    });
+}
+
+function rondo1(){
+    document.getElementById('bok').innerHTML+='<img src="roundabout.png" class="moving" height="160"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
+
+function skrzyz1(){
+    document.getElementById('bok').innerHTML+='<img src="crossing.png" class="moving" height="135"></img>';
+    $( function() {     
+        $( ".moving" ).draggable();
+      });
+}
